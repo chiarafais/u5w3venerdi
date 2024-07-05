3c69f73d9153a7536d8545551f06d5af9d204757
chiarafais <chiara0915@gmail.com>
2024-07-05 10:52:14 +0200

    first commit

---
+++ src/main/java/chiarafais/u5w3venerdi/U5w3venerdiApplication.java
@@ -0,0 +1,13 @@
+package chiarafais.u5w3venerdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w3venerdiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w3venerdiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chiarafais/u5w3venerdi/U5w3venerdiApplicationTests.java
@@ -0,0 +1,13 @@
+package chiarafais.u5w3venerdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w3venerdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
